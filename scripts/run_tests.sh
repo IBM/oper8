7a142e79d69aa321f5214826c272bbe21f69a74a
Gabe Goodhart <ghart@us.ibm.com>
2023-11-20 16:24:08 -0700

    LibraryPort: Allow DeprecationWarning when running with 3.12
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -12,6 +12,13 @@ else
     # NOTE: The AnsibleWatchManager raises an ImportWarning when ansible imports
     #   systemd under the hood
     warn_arg="-W error -W ignore::ImportWarning"
+
+    # If running with 3.12 or later, some of the dependencies use deprecated
+    # functionality
+    if [ "$(python --version | cut -d' ' -f 2 | cut -d'.' -f 2)" -gt "11" ]
+    then
+        warn_arg="$warn_arg -W ignore::DeprecationWarning"
+    fi
 fi
 
 PYTHONPATH="${BASE_DIR}:$PYTHONPATH" python3 -m pytest \
