f1e54d009fd4061cdfbcbf68f107516f5772c4fd
Gabe Goodhart <ghart@us.ibm.com>
2023-11-26 18:50:38 -0700

    FixRuffLinting: Only lint python files in the oper8 dir
    https://github.com/IBM/oper8/issues/12
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,6 +2,19 @@
 
 cd $(dirname ${BASH_SOURCE[0]})/..
 
-arg=${1:-"oper8"}
-shift
-ruff check $arg $@
+fnames=""
+for fname in $@
+do
+    if [[ "$fname" == *".py" ]] || [ -d $fname ] && [[ "$fname" == "oper8"* ]]
+    then
+        fnames="$fnames $fname"
+    else
+        echo "Ignoring non-library file: $fname"
+    fi
+done
+if [ "$fnames" == "" ]
+then
+    fnames="oper8"
+fi
+
+ruff check $arg $fnames
