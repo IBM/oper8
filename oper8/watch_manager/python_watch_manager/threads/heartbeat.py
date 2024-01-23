48fbeec1fd998599af58fef67775401cbc7619cb
Gabe Goodhart <ghart@us.ibm.com>
2024-01-23 15:08:19 -0700

    PWMProbes: Fix lifecycle and error handling for HeartbeatThread
    https://github.com/IBM/oper8/issues/38
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- oper8/watch_manager/python_watch_manager/threads/heartbeat.py
+++ oper8/watch_manager/python_watch_manager/threads/heartbeat.py
@@ -3,12 +3,17 @@ Thread class that will dump a heartbeat to a file periodically
 """
 
 # Standard
-from datetime import datetime, timedelta
+from datetime import datetime
+import threading
+
+# First Party
+import alog
 
 # Local
-from ....exceptions import assert_config
 from ..utils import parse_time_delta
 from .timer import TimerThread
+
+log = alog.use_channel("HBEAT")
 
 
 class HeartbeatThread(TimerThread):
@@ -37,16 +42,42 @@ class HeartbeatThread(TimerThread):
         """
         self._heartbeat_file = heartbeat_file
         self._offset = parse_time_delta(heartbeat_period)
-        assert_config(
-            self._offset >= timedelta(seconds=1),
-            "heartbeat_period must be >= 1s",
-        )
+        self._beat_lock = threading.Lock()
+        self._beat_event = threading.Event()
         super().__init__(name="heartbeat_thread")
-        self.put_event(datetime.now(), self._run_heartbeat)
+
+    def run(self):
+        self._run_heartbeat()
+        return super().run()
+
+    def wait_for_beat(self):
+        """Wait for the next beat"""
+        # Make sure the beat lock is not held before starting wait. This
+        # prevents beats that are immediately ready
+        with self._beat_lock:
+            pass
+
+        # Wait for the next beat
+        self._beat_event.wait()
 
     def _run_heartbeat(self):
         """Run the heartbeat dump to the heartbeat file and put the next beat"""
         now = datetime.now()
-        with open(self._heartbeat_file, "w") as handle:
-            handle.write(now.strftime(self._DATE_FORMAT))
-        self.put_event(now + self._offset, self._run_heartbeat)
+        log.debug3("Heartbeat %s", now)
+
+        # Save the beat to disk
+        try:
+            with open(self._heartbeat_file, "w", encoding="utf-8") as handle:
+                handle.write(now.strftime(self._DATE_FORMAT))
+                handle.flush()
+        except Exception as err:
+            log.warning("Failed to write heartbeat file: %s", err, exc_info=True)
+
+        # Unblock and reset the wait condition
+        with self._beat_lock:
+            self._beat_event.set()
+            self._beat_event.clear()
+
+        # Put the next beat if not stopped
+        if not self.should_stop():
+            self.put_event(now + self._offset, self._run_heartbeat)
